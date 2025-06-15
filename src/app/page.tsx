3e5946defca1a2d6eacf462cf901aa88e421bffa
JostinDev <justinnydegger@hotmail.com>
2025-06-15 21:01:37 +0200

    Font changes on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
   if (positions.length === 0 || offsets.length === 0) return null;
 
   return (
-    <div>
+    <div className="container mx-auto px-5">
       <div className="fixed inset-0 pointer-events-none z-50">
         {foodIcons.map((src, index) => (
           <div key={index}>
@@ -139,10 +139,10 @@ export default function Home() {
       </div>
 
       <div className="flex items-center flex-col justify-center h-screen">
-        <h1 className="text-7xl mb-8 text-center font-ptSerif font-bold text-title">
+        <h1 className="md:text-7xl sm:text-6xl text-5xl mb-8 text-center font-ptSerif font-bold text-title">
           Welcome to ezrecipe
         </h1>
-        <h2 className="text-3xl text-center font-inter text-body">
+        <h2 className="md:text-3xl sm:text-2xl text-xl text-center font-inter text-body">
           Your no-bullshit recipes saver
         </h2>
         <button className="bg-pastelYellow text-title rounded-xl mt-10 px-8 py-4 font-inter font-bold border-2 transition border-title hover:drop-shadow-[4px_4px_0px] drop-shadow-shadow">
